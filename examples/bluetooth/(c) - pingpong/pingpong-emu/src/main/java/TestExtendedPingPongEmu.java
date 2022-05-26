821eede81bf581379a7b56b59526437eb2b63281
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2022-05-26 12:58:47 +0200

    Add bluetooth example and serialComChannel

--- examples/bluetooth/(c) - pingpong/pingpong-emu/src/main/java/TestExtendedPingPongEmu.java
+++ examples/bluetooth/(c) - pingpong/pingpong-emu/src/main/java/TestExtendedPingPongEmu.java
@@ -26,36 +26,6 @@ public class TestExtendedPingPongEmu {
         System.out.println("Waiting Arduino for rebooting...");
         Thread.sleep(4000);
         System.out.println("Ready.");
-
-        /*
-         * Socket used to test when no emu is running
-         */
-		/*
-		Socket socket = new Socket("localhost",8080);
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		 */
-        //while (true) {
-            /*
-             *  Socket used to test when no emu is running
-             */
-			/*
-			System.out.println("Sending ping on BT emu channel...");
-			out.write("ping$\n");
-			out.flush();
-
-			String msg = in.readLine();
-			System.out.println("Received: " + msg);
-			Thread.sleep(500);
-			*/
-
-//            System.out.println("Sending ping on the serial channel...");
-//            channel.sendMsg("ping");
-//            String msg = channel.receiveMsg();
-//            System.out.println("Received: "+msg);
-//            Thread.sleep(500);
-
-        //}
     }
 
 }
